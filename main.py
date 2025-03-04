c342660068eec775329ac8fb4ced70d39169a879
geime <exmpl@>
2025-03-04 17:59:53 +0300

    Task "Колонизация Марса"

--- main.py
+++ main.py
@@ -2,5 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def main():
+    return '<h1>Миссия Колонизация Марса</h1>'
+
+
+@app.route('/index')
+def index():
+    return '<h2>И на Марсе будут яблони цвести!</h2>'
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
