1ca5ce92d50f8d3af615f698e8697120e8ae6214
IT Куб Python 2 <example@gmail.com>
2025-03-06 18:09:15 +0300

    task5

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super_dooper_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя:', validators=[DataRequired()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    password_repeat = PasswordField('Повтор пароля:', validators=[EqualTo(password)])
+    submit = SubmitField('Отправить')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 
 @app.route('/<title>')
@@ -19,20 +35,6 @@ def training(prof):
 def list_prof(list):
     profs = ['инженер', 'строитель', "врач", "водитель", "биолог", "геодезист"]
     return render_template('list_prof.html', list=list, profs=profs)
-
-
-@app.route('/answer')
-@app.route('/auto_answer')
-def answer():
-    answ_dict = {"title": "Анкета",
-                 "surname": "Сева",
-                 "name": "Иван",
-                 "education": "Среднее общее",
-                 "profession": "Геодезист",
-                 "sex": "Мужской",
-                 "motivation": "Меня папа позвал",
-                 "ready": True}
-    return render_template('auto_answer.html', **answ_dict)
 
 
 if __name__ == '__main__':
