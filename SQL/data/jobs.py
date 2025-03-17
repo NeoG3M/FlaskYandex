c4ece5f7bb3bf809650306df12569bb3b16b508f
IT Куб Python 2 <example@gmail.com>
2025-03-17 17:16:53 +0300

    Task "Модель Работы"

---
+++ SQL/data/jobs.py
@@ -0,0 +1,22 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy.util.preloaded import orm
+
+from db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, unique=True)
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    user = orm.relationship('User')
